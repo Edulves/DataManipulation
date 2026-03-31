6745f5f4ef76d32719dc50502680060c5d1795ca
Eduardo Alves <Eduardo.lves@outlook.com>
2026-03-31 16:01:38 -0300

    Percorrendo coleções

---
+++ IntroAColecoes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+var diasDaSemana = new string[] { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+
+var carrinho = new List<Produto>()
+{
+    new("Leite", 7.89),
+    new("Manteiga",3.45)
+    //"Domingo"
+};
+
+// diasDaSemana[0];
+//carrinho[0];
+
+
+
+
+PercorrendoComForEach();
+
+void PercorrendoComFor()
+{
+    for (int i = 0; i < carrinho.Count; i++)
+    {
+        var produto = carrinho[i];
+        Console.WriteLine($"Produto: {produto}");
+    }
+}
+
+void PercorrendoComForEach()
+{
+    foreach (var produto in carrinho)
+    {
+        Console.WriteLine($"Produto: {produto.Nome}");
+    }
+}
+
+class Produto(string nome, double preco)
+{
+    public string Nome { get; } = nome;
+    public double Preco { get; } = preco;
+}
