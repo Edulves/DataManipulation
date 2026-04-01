f14ed2fbb1a5586ab9ee4abeb7dad9653db37d8d
Eduardo Alves <Eduardo.lves@outlook.com>
2026-04-01 16:59:36 -0300

    Playlist de músicas

---
+++ 02-OperacoesEmColecoes/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+   Seja um aplicativo de gerenciamento de músicas onde os usuários podem organizar suas faixas favoritas em playlists personalizadas. Para cada playlist, é essencial que o usuário tenha controle total sobre a sequência de reprodução das músicas, permitindo reordená-las livremente a qualquer momento. Além disso, o aplicativo precisa oferecer a funcionalidade de reprodução aleatória para uma playlist específica, proporcionando uma experiência de audição dinâmica e variada, sem, contudo, alterar a ordem original que o usuário definiu. O desafio é criar uma estrutura robusta que suporte a adição e remoção eficiente de músicas, a reordenação flexível dentro das playlists e a seleção de faixas tanto em modo sequencial quanto aleatório.
+
+   Funções que vamos implementar:
+   //     [x] Criar as classes para musicas e playlist
+   //     [ ] Listar músicas da playlist
+   //     [ ] Adicionar música à playlist
+   //     [ ] Obter uma música específica da playlist
+   //     [ ] Remover música da playlist
+   //     [ ] Tocar músicas da playlist em modo aleatório 
+   //     [ ] Reordenar músicas segundo alguma lógica específica (ex. duração)
+   //     [ ] Uma playlist não pode ter músicas repetidas
+   //     [ ] Exibir as 10 músicas mais tocadas em todas as playlists (ranking)
+   //     [ ] Player de música com:
+   //     [ ] - Fila de reprodução (para músicas avulsas e/ou playlists)
+   //     [ ] - Histórico de reprodução
+
+*/
+
+using System.Collections;
+
+var musica1 = new Musica { Titulo = "Que País é esse?", Artista = "Legião Urbana", Duracao = 350 };
+var musica2 = new Musica { Titulo = "Tempo Perdido", Artista = "Legião Urbana", Duracao = 455 };
+var musica3 = new Musica { Titulo = "Pro Dia Nascer Feliz", Artista = "Barão Vermelho", Duracao = 345 };
+var musica4 = new Musica { Titulo = "Eduardo e Mônica", Artista = "Legião Urbana", Duracao = 530 };
+var musica5 = new Musica { Titulo = "Geração Coca-Cola", Artista = "Legião Urbana", Duracao = 350 };
+
+var rockNacional = new Playlist { Nome = "Rock Nacional" };
+rockNacional.Add(musica1);
+rockNacional.Add(musica2);
+rockNacional.Add(musica3);
+rockNacional.Add(musica4);
+rockNacional.Add(musica5);
+
+ExibirPlaylist(rockNacional);
+
+void ExibirPlaylist(Playlist playlist)
+{
+    Console.WriteLine($"\n Tocando as músicas de {playlist.Nome}");
+    foreach (var musica in playlist)
+    {
+        Console.WriteLine($"\t - {musica.Titulo}");
+    }
+}
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+}
+
+class Playlist : ICollection<Musica>
+{
+    private List<Musica> lista = [];
+    public string Nome { get; set; }
+
+    public int Count => lista.Count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(Musica musica)
+    {
+        lista.Add(musica);
+    }
+
+    public void Clear()
+    {
+        lista.Clear();
+    }
+
+    public bool Contains(Musica item)
+    {
+        return lista.Contains(item);
+    }
+
+    public void CopyTo(Musica[] array, int arrayIndex)
+    {
+        lista.CopyTo(array, arrayIndex);
+    }
+
+    public IEnumerator<Musica> GetEnumerator()
+    {
+        return lista.GetEnumerator();
+    }
+
+    public bool Remove(Musica item)
+    {
+        return lista.Remove(item);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
--- IntroAColecoes/Program.cs
+++ IntroAColecoes/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 
-var diasDaSemana = new string[] { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+var diasDaSemana = new DiasDaSemana();
 
 var carrinho = new List<Produto>()
 {
@@ -9,13 +9,62 @@ var carrinho = new List<Produto>()
     //"Domingo"
 };
 
-// diasDaSemana[0];
-//carrinho[0];
+var pares = NumerosParesComYield();
+var contador = 0;
+foreach (var par in pares) 
+{ 
+    contador++;
+    Console.WriteLine(par);
+    if(contador > 200) break;
+} 
+
+IEnumerable<int> NumerosParesSemYield(int limite)
+{
+    var lista = new List<int>();
+    for (var i = 0; i < limite; i++)
+    {
+        Console.WriteLine($"Processando elemento {i}...");
+        lista.Add(i * 2); 
+    }
+    return lista;
+}
+
+IEnumerable<int> NumerosParesComYield()
+{
+    var i = 0;
+    while(true)
+    {
+        Console.WriteLine($"Processando elemento {i}...");
+        yield return i * 2; //arquivo
+        if (i > 100)
+        {
+            Console.WriteLine("Saindo do enumerator!");
+            yield break;
+        }
+        i++;
+    }
+}
+
+void PercorrendoComEnumerator()
+{
+    var enumerator = diasDaSemana.GetEnumerator();
+    while (enumerator.MoveNext())
+    {
+        var dia = enumerator.Current;
+        Console.WriteLine(dia);
+    }
+}
+
+void PercorrendoDiasDaSemana()
+{
+    foreach (var dia in diasDaSemana)
+    {
+        Console.WriteLine(dia);
+    }
+}
 
 
-
-
-PercorrendoComForEach();
+//PercorrendoComForEach();
 
 void PercorrendoComFor()
 {
@@ -38,4 +87,47 @@ class Produto(string nome, double preco)
 {
     public string Nome { get; } = nome;
     public double Preco { get; } = preco;
+}
+
+class DiasDaSemanaEnumerator : IEnumerator<string>
+{
+    private int posicao = -1;
+    private string[] dias = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado" };
+    public string Current => dias[posicao];
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+
+    public bool MoveNext()
+    {
+        posicao++;
+        return posicao < dias.Length;
+    }
+
+    public void Reset()
+    {
+        posicao = -1;
+    }
+}
+
+class DiasDaSemana : IEnumerable<string>
+{
+    public IEnumerator<string> GetEnumerator()
+    {
+        yield return "Domingo";
+        yield return "Segunda";
+        yield return "Terça";
+        yield return "Quarta";
+        yield return "Quinta";
+        yield return "Sexta";
+        yield return "Sábado";
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 }
