e878235b6be79e741c33c6e66e84d3d2a208dc77
Eduardo Alves <Eduardo.lves@outlook.com>
2026-04-07 16:09:42 -0300

    Obtenção de elementos específicos

--- 04-LINQ/Program.cs
+++ 04-LINQ/Program.cs
@@ -4,13 +4,13 @@
     Implemente as funções abaixo:
     //     [x] Leia-o como uma coleção de músicas
     //     [x] Filtre a coleção por artista (por ex. Coldplay, Metallica, AC/DC)
-    //     [ ] Filtre a coleção por gênero (por ex. rock)
-    //     [ ] Filtre a coleção por duração (por ex. maiores que 5 minutos)
-    //     [ ] Ordene a coleção por artista
-    //     [ ] Ordene a coleção por artista e em seguida por músicas com duração crescente
-    //     [ ] Crie uma coleção de artistas e suas músicas
-    //     [ ] Informe a duração média das músicas da coleção
-    //     [ ] Informe a duração total das músicas da coleção
+    //     [x] Filtre a coleção por gênero (por ex. rock)
+    //     [x] Filtre a coleção por duração (por ex. maiores que 5 minutos)
+    //     [x] Ordene a coleção por artista
+    //     [x] Ordene a coleção por artista e em seguida por músicas com duração crescente
+    //     [x] Crie uma coleção de artistas e suas músicas
+    //     [x] Informe a duração média das músicas da coleção
+    //     [x] Informe a duração total das músicas da coleção
     //     [ ] Informe qual artista tem mais músicas na coleção
  
 */
@@ -18,18 +18,95 @@
 using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
 using var stream = new StreamReader(arquivo);
 
-var musicasDoColdplay =
-    (ObterMusicas(stream))                  // 1. obtenção de dados
-    .Where(musica => musica.Artista == "Coldplay")        // 2. filtragem por artista
-    .OrderBy(musica => musica.Titulo)
-    .ThenBy(musica => musica.Duracao)
-    .Skip(5 * 2)
-    .Take(5);
-
-ExibirMusicas(musicasDoColdplay);
+var artista = ObterMusicas(stream)
+    .GroupBy(m => m.Artista)
+    .Select(g => new { Artista = g.Key, musica = g, Total = g.Count() }
+    ).MaxBy(a => a.Total);
 
 
-void ExibirMusicas(IEnumerable<Musica> musicas)
+Console.WriteLine($"O artista com maior qtde de músicas é o {artista.Artista} com {artista.Total}!");
+
+void OperacoesDeObtencaiDeElementos(StreamReader stream)
+{
+    var musicas = ObterMusicas(stream);
+
+    var primeiraMusica = musicas.First();
+    Console.WriteLine($"A primeira música é {primeiraMusica.Titulo}");
+
+    var maiorDuracao = musicas.MaxBy(m => m.Duracao);
+    if(maiorDuracao is not null)
+        Console.WriteLine($"A música com maior duração é {maiorDuracao.Titulo} com {maiorDuracao.Duracao} segundos.");
+}
+
+
+void OpercaoesDeAgrupamento(StreamReader stream)
+{
+    var artistas = ObterMusicas(stream)
+    .GroupBy(m => m.Artista);
+
+    Console.WriteLine($"\nExibindo as músicas de cada artista:");
+    foreach (var artista in artistas.Take(5))
+    {
+        Console.WriteLine($"Artista: {artista.Key} com {artista.Count()} músicas");
+        foreach (var musica in artista)
+        {
+            Console.WriteLine($"\t - {musica.Titulo}");
+        }
+    }
+}
+
+void EstatisticasDeMusicas(StreamReader stream)
+{
+    var musicas = ObterMusicas(stream).ToList();
+
+    Console.WriteLine($"\nExistem {musicas.Count()} músicas na coleção.");
+    Console.WriteLine($"\nExistem {musicas.Count(m => m.Duracao >= 600)} músicas com mais do que 10 minutos de duração na coleção.");
+    Console.WriteLine($"\nA música com menor duração da coleção tem {musicas.Min(m => m.Duracao)} segundos.");
+    Console.WriteLine($"\nA música com maior duração da coleção tem {musicas.Max(m => m.Duracao)} segudnos.");
+    Console.WriteLine($"\nA duração média das músicas da coleção é {musicas.Average(m => m.Duracao).ToString("F2")} segundos.");
+    Console.WriteLine($"\nVocê vai levar {musicas.Sum(m => m.Duracao)/(60 * 60 * 24)} dias para ouvir toda a coleção.");
+}
+
+void OperacoesDeProjecao2(StreamReader stream)
+{
+    var generos = ObterMusicas(stream)
+        .SelectMany(m => m.Generos)
+        .Distinct()
+        .OrderBy(g => g);
+
+    foreach (var genero in generos)
+    {
+        Console.WriteLine(genero);
+    }
+}
+
+void OperacoesDeProjecao(StreamReader stream)
+{
+    var artistas = ObterMusicas(stream)
+    .Select(m => m.Artista)
+    .Distinct()
+    .OrderBy(a => a);
+
+    foreach (var artist in artistas)
+    {
+        Console.WriteLine(artist);
+    }
+}
+
+
+void OperacoesDeFiltroEOrdenacao(StreamReader stream)
+{
+    var musicasDoColdplay =
+        (ObterMusicas(stream))
+        .Where(musica => musica.Artista == "Coldplay")
+        .OrderBy(musica => musica.Titulo)
+        .Skip(5 * 2)
+        .Take(5);
+
+    ExibirMusicas(musicasDoColdplay);
+}
+
+void ExibirMusicas(IEnumerable<Musica> musicas) 
 {
     var contador = 1;
     Console.WriteLine($"\nExibindo as músicas");
@@ -55,7 +132,8 @@ IEnumerable<Musica> ObterMusicas(StreamReader stream)
         {
             Titulo = partes[0],
             Artista = partes[1],
-            Duracao = int.Parse(partes[2])
+            Duracao = int.Parse(partes[2]),
+            Generos = partes[3].Split(",").Select(g => g.Trim())
         };
         yield return musica;
         linha = stream.ReadLine();
@@ -67,4 +145,5 @@ class Musica
     public string Titulo { get; set; }
     public string Artista { get; set; }
     public int Duracao { get; set; }
+    public IEnumerable<string> Generos { get; set; }
 }
